d8fda32e96cfb4b8790f0a79a1d73a23c9b447d3
Ailton Costa <166161526+acadl-dev@users.noreply.github.com>
2025-09-14 16:46:36 -0300

    Adicionado a atividades 5 e 6

---
+++ TP2.5/CreateCity/Pages/CreateCountry.cshtml.cs
@@ -0,0 +1,54 @@
+using CreateCity.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using static CreateCity.Pages.CityManager.CreateCityModel;
+
+namespace CreateCity.Pages
+{
+    public class CreateCountryModel : PageModel
+    {
+        [BindProperty]
+        public InputModel Input { get; set; } = new();
+
+        // Aqui vou armazenar os países temporariamente
+        public static List<Country> Countries { get; set; } = new();
+
+        public void OnGet()
+        {
+        }
+
+        // Método chamado ao submeter o formulário
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page(); // Retorna a página com mensagens de validação
+            }
+
+            // Criando uma instância de Country a partir do InputModel
+            Country newCountry = new Country
+            {
+                Id = Countries.Count + 1, // Apenas como exemplo, gera um Id incremental
+                CountryName = Input.CountryName,
+                CountryCode = Input.CountryCode
+            };
+
+            Countries.Add(newCountry); // Adiciona à lista (em um cenário real, salvar no banco)
+            ModelState.Clear();
+
+            return Page();
+        }
+
+        public class InputModel
+        {
+            [Required(ErrorMessage = "Nome do país é obrigatório")]
+            [MinLength(2, ErrorMessage = "Nome do país deve ter no mínimo 2 caracteres")]
+            public string CountryName { get; set; }
+
+            [Required(ErrorMessage = "Código do país é obrigatório")]
+            [MinLength(2, ErrorMessage = "Código do país deve ter no mínimo 2 caracteres")]
+            public string CountryCode { get; set; }
+        }
+    }
+}
